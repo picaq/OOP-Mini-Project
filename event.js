357a8c435fcb151926a65f501386a912464b0310
Mandy <34908590+picaq@users.noreply.github.com>
2021-02-17 14:59:21 -0800

    4. create an object and assign values

--- event.js
+++ event.js
@@ -4,4 +4,10 @@ class Event {
       this.description = description;
       this.availableTickets = [];
     }
-  }
+}
+
+// The below statement creates an object.
+const eventObj1 = new Event(
+    'KLOS Golden Gala',
+    'An evening with hollywood vampires'
+);
