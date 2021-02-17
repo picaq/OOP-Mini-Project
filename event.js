a86ffe083c33a10cfcf32b177231b1ce9707293c
Mandy <34908590+picaq@users.noreply.github.com>
2021-02-17 15:28:39 -0800

    7. push objects into array, and 8. check if pushed

--- event.js
+++ event.js
@@ -18,3 +18,11 @@ const eventObj3 = new Event('Jenny Lewis', 'On the line tour 2019');
 
 // 6. Create an empty Event array.
 const eventArray = new Array();
+
+// 7. pushing single object to an array
+eventArray.push(eventObj1);
+// pushing multiple objects to an array at once
+eventArray.push(eventObj1, eventObj2, eventObj3);
+
+// 8. in order to check whether the elements are pushed, use console.log
+console.log(eventArray);
