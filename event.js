db8814eb3b9ed3258ba531692bdbd7e9a8ad945b
Mandy <34908590+picaq@users.noreply.github.com>
2021-02-18 11:59:10 -0800

    create TicketType class, adjust indentation overall

--- event.js
+++ event.js
@@ -1,15 +1,16 @@
 class Event {
-    constructor(name, description) {
-      this.name = name;
-      this.description = description;
-      this.availableTickets = [];
-    }
+  constructor(name, description) {
+    this.name = name;
+    this.description = description;
+    this.availableTickets = [];
+  }
+    
 }
 
 // The below statement creates an object.
 const eventObj1 = new Event(
-    'KLOS Golden Gala',
-    'An evening with hollywood vampires'
+  'KLOS Golden Gala',
+  'An evening with hollywood vampires'
 );
 
 // 5. Create few more objects with different values.
@@ -29,10 +30,18 @@ console.log(eventArray);
 
 // 9. jQuery to iterate through array
 $(document).ready(function () {
-    let html = '';
-    $.each(eventArray, function (index, item) {
-      html += `<li>${item.name} - ${item.description}</li>`;
-    });
-    // insert final html into #event...
-    $('#event').html(html);
+  let html = '';
+  $.each(eventArray, function (index, item) {
+    html += `<li>${item.name} - ${item.description}</li>`;
   });
+  // insert final html into #event...
+  $('#event').html(html);
+});
+
+// 2. create class TicketType, name of ticket type, price
+class TicketType {
+  constructor(name, price) {
+    this.name = name;
+    this.price = price;
+  }
+}
